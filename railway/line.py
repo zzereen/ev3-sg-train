12087219b3fe23403400e00ef57da3a962e54961
Unknown <omfgitsnotshaz@gmail.com>
2017-03-14 07:15:08 +0800

    Move line stations to individual line declaration

--- railway/line.py
+++ railway/line.py
@@ -8,6 +8,46 @@ class Line(object):
         self.color = color
         self.stations = stations
 
-EWL_Line = Line(color=GREEN, stations=EWL_STATIONS)
-NSL_Line = Line(color=RED, stations=NSL_STATIONS)
-CCL_Line = Line(color=YELLOW, stations=CCL_STATIONS)
+EWL = Line(color=color.GREEN, stations=[
+    station.JURONG_EAST,
+    station.CLEMENTI,
+    station.BUONA_VISTA,
+    station.OUTRAM_PARK,
+    station.CITY_HALL,
+    station.BUGIS,
+    station.KALLANG,
+    station.PAYA_LEBAR,
+    station.BEDOK,
+    station.TANAH_MERAH,
+    station.PASIR_RIS,
+    station.CHANGI
+])
+
+NSL = Line(color=color.RED, stations=[
+    station.JURONG_EAST,
+    station.CHUA_CHU_KANG,
+    station.WOODLANDS,
+    station.YISHUN,
+    station.BISHAN,
+    station.TOA_PAYOH,
+    station.NEWTON,
+    station.ORCHARD,
+    station.SOMERSET,
+    station.DHOBY_GHAUT,
+    station.CITY_HALL,
+    station.MARINA_BAY
+])
+
+CCL = Line(color=color.YELLOW, stations=[
+    station.HABOURFRONT,
+    station.HAW_PAR_VILLA,
+    station.BUONA_VISTA,
+    station.HOLLAND_VILLAGE,
+    station.BOTANIC_GARDENS,
+    station.BISHAN,
+    station.SERANGOON,
+    station.PAYA_LEBAR,
+    station.STADIUM,
+    station.ESPLANADE,
+    station.DHOBY_GHAUT
+])
