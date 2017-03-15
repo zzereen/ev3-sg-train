e6d7e5d04b8b1a6a84af341cc470f01245d145cc
Muhammad Shazrin <omfgitsnotshaz@gmail.com>
2017-03-15 21:11:29 +0800

    Helper methods to extract info of stations on each line

--- railway/line.py
+++ railway/line.py
@@ -7,7 +7,25 @@ class Line(object):
     def __init__(self, name: str, color: 'Color', stations: 'List[Station]'):
         self.name = name
         self.color = color
-        self.stations = stationsclass MRTLine(object):
+        self.stations = stations
+
+    def get_station_index(self, target_station: 'Station') -> int:
+        for i in range(0, len(self.stations)):
+            if self.stations[i] == target_station:
+                return i
+
+        return -1
+
+    def get_intersecting_stations(self, target_line: 'Line') -> 'List[Station]':
+        intersecting_stations = []
+
+        for station_curr in self.stations:
+            for station_tgt in target_line.stations:
+                if station_curr == station_tgt:
+                    intersecting_stations.append(station_curr)
+
+        return intersecting_stations
+
 class MRTLine(object):
     EWL = Line(name="East West Line", color=Color.GREEN, stations=[
         MRTStation.JURONG_EAST,
