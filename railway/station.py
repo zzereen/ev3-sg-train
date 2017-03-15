e8d9d0d4e1fad4b0a2641bf23cce73952f457408
Muhammad Shazrin <omfgitsnotshaz@gmail.com>
2017-03-15 21:09:15 +0800

    Replace StationPath with line_flow

--- railway/station.py
+++ railway/station.py
@@ -1,16 +1,11 @@
+from typing import Dict
+from utils.direction import Direction
+
 class Station(object):
     def __init__(self, name: str):
         self.name = name
-        self.EWL_station_path: 'StationPath' = None
-        self.NSL_station_path: 'StationPath' = None
-        self.CCL_station_path: 'StationPath' = None
+        self.line_flow: 'Dict[Line, Direction]' = {}
 
-class StationPath(object):
-    def __init__(self, up: 'Station' = None, right: 'Station' = None, left: 'Station' = None, down: 'Station' = None):
-        self.up = up
-        self.right = right
-        self.down = down
-        self.left = left
 class MRTStation(object):
     CHANGI = Station("Changi")
     PAYA_LEBAR = Station("Paya Lebar")
