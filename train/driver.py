497415d545f0e294160dbc1ee6441a2f54b65b67
Muhammad Shazrin <omfgitsnotshaz@gmail.com>
2017-03-16 16:16:52 +0800

    Replace Driver & listener and MRTDriver & listener

--- train/driver.py
+++ train/driver.py
@@ -1,18 +1,14 @@
 from railway.station import Station
-from railway.line import Line
+from railway.line import Line, MRTLine
 from train.train import Train, TrainListener
+from utils.direction import TurnDirection
 
-class DriverListener(object):
-    def on_station_reached(self, station: 'Station', line: 'Line'):
-        pass
+class MRTDriver(TrainListener):
+    def __init__(self, route: 'Route'):
+        self.route = route
+        self.current_MRT_line = route.start_line
+        self.listeners = []
 
-    def on_line_change(self, station: 'Station', curr_line: 'Line', target_line: 'Line'):
-        pass
-
-    def on_end_station_reached(self, station: 'Station', line: 'Line'):
-        pass
-
-class BaseDriver(TrainListener):
     def on_black(self, train: 'Train', left: bool, right: bool):
         train.stop()
 
@@ -20,13 +16,25 @@ class BaseDriver(TrainListener):
         train.stop()
 
     def on_green(self, train: 'Train', left: bool, right: bool):
-        train.stop()
+        if self.current_MRT_line == MRTLine.EWL:
+            if left:
+                train.steer_left()
+            elif right:
+                train.steer_right()
 
     def on_yellow(self, train: 'Train', left: bool, right: bool):
-        train.stop()
+        if self.current_MRT_line == MRTLine.CCL:
+            if left:
+                train.steer_left()
+            elif right:
+                train.steer_right()
 
     def on_red(self, train: 'Train', left: bool, right: bool):
-        train.stop()
+        if self.current_MRT_line == MRTLine.NSL:
+            if left:
+                train.steer_left()
+            elif right:
+                train.steer_right()
 
     def on_white(self, train: 'Train', left: bool, right: bool):
         if left and right:
@@ -35,23 +43,37 @@ class BaseDriver(TrainListener):
     def on_brown(self, train: 'Train', left: bool, right: bool):
         train.stop()
 
-class EWLDriver(BaseDriver):
-    def on_green(self, train: 'Train', left: bool, right: bool):
-        if left:
-            train.turn_left()
-        elif right:
-            train.turn_right()
+    def on_click(self, train: 'Train'):
+        station = self.route.path.pop(0)
 
-class NSLDriver(BaseDriver):
-    def on_red(self, train: 'Train', left: bool, right: bool):
-        if left:
-            train.turn_left()
-        if right:
-            train.turn_right()
+        for listener in self.listeners:
+            listener.on_station_reached(station, self.current_MRT_line)
 
-class CCLDriver(BaseDriver):
-    def on_yellow(self, train: 'Train', left: bool, right: bool):
-        if left:
-            train.turn_left()
-        elif right:
-            train.turn_right()
+        if station == self.route.end_station:
+            train.stop()
+
+            for listener in self.listeners:
+                listener.on_end_station_reached(station, self.current_MRT_line)
+
+        if station == self.route.transfer_station:
+            if self.route.transfer_turn_direction == TurnDirection.LEFT:
+                train.steer_left()
+            elif self.route.transfer_turn_direction == TurnDirection.RIGHT:
+                train.steer_right()
+
+            self.current_MRT_line = self.route.end_line
+
+        train.move_forward()
+
+    def add_listener(self, listener: 'MRTDriverListener'):
+        self.listeners.append(listener)
+
+class MRTDriverListener(object):
+    def on_station_reached(self, station: 'Station', line: 'Line'):
+        pass
+
+    def on_line_change(self, station: 'Station', curr_line: 'Line', target_line: 'Line'):
+        pass
+
+    def on_end_station_reached(self, station: 'Station', line: 'Line'):
+        pass
