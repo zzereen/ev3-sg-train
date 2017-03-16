df5e564482f176d7cb2779f419bd3f8b322bf08b
Muhammad Shazrin <omfgitsnotshaz@gmail.com>
2017-03-16 16:15:59 +0800

    Add support for multiple listeners

--- train/train.py
+++ train/train.py
@@ -12,7 +12,7 @@ class Train(object):
         self.left_large_motor = LargeMotor('outD')
         self.right_large_motor = LargeMotor('outA')
         self.touch_sensor = TouchSensor('in3')
-        self.listener = None
+        self.listeners = []
         self.prev_is_pressed = False
 
     def move_forward(self):
@@ -43,38 +43,46 @@ class Train(object):
         self.left_large_motor.stop()
         self.right_large_motor.stop()
 
-    def set_listener(self, train_listener: 'TrainListener'):
-        self.listener = train_listener
-
     def step(self):
         left_color = self.left_color_sensor.color
         right_color = self.right_color_sensor.color
 
         if left_color == Color.BLACK or right_color == Color.BLACK:
-            self.listener.on_black(self, left_color == Color.BLACK, right_color == Color.BLACK)
+            for listener in self.listeners:
+                listener.on_black(self, left_color == Color.BLACK, right_color == Color.BLACK)
 
         if left_color == Color.BLUE or right_color == Color.BLUE:
-            self.listener.on_blue(self, left_color == Color.BLUE, right_color == Color.BLUE)
+            for listener in self.listeners:
+                listener.on_blue(self, left_color == Color.BLUE, right_color == Color.BLUE)
 
         if left_color == Color.GREEN or right_color == Color.GREEN:
-            self.listener.on_green(self, left_color == Color.GREEN, right_color == Color.GREEN)
+            for listener in self.listeners:
+                listener.on_green(self, left_color == Color.GREEN, right_color == Color.GREEN)
 
         if left_color == Color.YELLOW or right_color == Color.YELLOW:
-            self.listener.on_yellow(self, left_color == Color.YELLOW, right_color == Color.YELLOW)
+            for listener in self.listener:
+                listener.on_yellow(self, left_color == Color.YELLOW, right_color == Color.YELLOW)
 
         if left_color == Color.RED or right_color == Color.RED:
-            self.listener.on_red(self, left_color == Color.RED, right_color == Color.RED)
+            for listener in self.listeners:
+                listener.on_red(self, left_color == Color.RED, right_color == Color.RED)
 
         if left_color == Color.WHITE or right_color == Color.WHITE:
-            self.listener.on_white(self, left_color == Color.WHITE, right_color == Color.WHITE)
+            for listener in self.listeners:
+                listener.on_white(self, left_color == Color.WHITE, right_color == Color.WHITE)
 
         if left_color == Color.BROWN or right_color == Color.BROWN:
-            self.listener.on_brown(self, left_color == Color.BROWN, right_color == Color.BROWN)
+            for listener in self.listeners:
+                listener.on_brown(self, left_color == Color.BROWN, right_color == Color.BROWN)
 
         if self.prev_is_pressed and not self.touch_sensor.is_pressed:
-            self.listener.on_click(self)
+            for listener in self.listeners:
+                listener.on_click(self)
 
         self.prev_is_pressed = self.touch_sensor.is_pressed
+
+    def add_listener(self, listener: 'TrainListener'):
+        self.listeners.append(listener)
 
 class TrainListener(object):
     def on_black(self, train: 'Train', left: bool, right: bool):
