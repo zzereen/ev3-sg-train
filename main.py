57b7fc0d514517d4ba8fec0193c8a74b2dd341c8
Muhammad Shazrin <omfgitsnotshaz@gmail.com>
2017-03-16 16:46:32 +0800

    Main worthy

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-from train.driver import EWLDriver, NSLDriver, CCLDriver
+from train.driver import MRTDriver
 from train.train import Train
+from train.route import Route
+from railway.line import MRTLine
+from railway.station import MRTStation
 
 if __name__ == "__main__":
+    route = Route(MRTStation.JURONG_EAST, MRTStation.CHANGI, MRTLine.EWL, MRTLine.EWL)
+
     train = Train()
-    train.set_listener(EWLDriver())
+    train.add_listener(MRTDriver(route))
 
     while True:
         train.step()
