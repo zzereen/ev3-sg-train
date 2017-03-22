f08601266223a552af145e4d8496bc4a68ba2d85
Muhammad Shazrin <omfgitsnotshaz@gmail.com>
2017-03-22 10:01:53 +0800

    Move route class to railway package

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
+from railway.line import MRTLine
+from railway.route import Route
+from railway.station import MRTStation
 from train.driver import MRTDriver
 from train.train import Train
-from train.route import Route
-from railway.line import MRTLine
-from railway.station import MRTStation
 
 if __name__ == "__main__":
     route = Route(MRTStation.JURONG_EAST, MRTStation.CHANGI, MRTLine.EWL, MRTLine.EWL)
