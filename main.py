53993f84d11361c9f825dab7f3f9c9d8b734ca1d
Muhammad Shazrin <omfgitsnotshaz@gmail.com>
2017-03-22 13:12:28 +0800

    A proper main worthy feature

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 from railway.line import MRTLine
 from railway.route import Route
 from railway.station import MRTStation
-from train.driver import MRTDriver
-from train.train import Train
+from vehicle.train import Train
 
 if __name__ == "__main__":
+    print("Started")
     route = Route(MRTStation.JURONG_EAST, MRTStation.CHANGI, MRTLine.EWL, MRTLine.EWL)
 
     train = Train()
-    train.add_listener(MRTDriver(route))
+    train.start(route)
 
-    while True:
-        train.step()
+    print("Ended")
