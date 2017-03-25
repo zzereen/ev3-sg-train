057894aff6cc53659bb101e590283a7b25653e6c
Muhammad Shazrin <omfgitsnotshaz@gmail.com>
2017-03-25 22:21:42 +0800

    Update temporary main

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
-from railway.line import MRTLine
 from railway.route import Route
-from railway.station import MRTStation
 from vehicle.train import Train
+from data.map import MRTStations, MRTLines
 
 if __name__ == "__main__":
     print("Started")
-    route = Route(MRTStation.JURONG_EAST, MRTStation.CHANGI, MRTLine.EWL, MRTLine.EWL)
+
+    start_station   = MRTStations.get_station_by_name("jurong east")
+    end_station     = MRTStations.get_station_by_name("marina bay")
+    trans_station   = MRTStations.get_station_by_name("city hall")
+    start_line      = MRTLines.get_line_by_name("ewl")
+    end_line        = MRTLines.get_line_by_name("nsl")
+
+    route = Route(start_station, end_station, start_line, end_line, trans_station)
 
     train = Train()
     train.start(route)
-
-    print("Ended")
