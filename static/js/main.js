6a6522bbc63a1dbc0801b4616ce11214079b49c1
Muhammad Shazrin <omfgitsnotshaz@gmail.com>
2017-03-29 21:51:28 +0800

    Webpage for UI

--- static/js/main.js
+++ static/js/main.js
@@ -4,9 +4,10 @@ window.onload = function () {
     xhr.onreadystatechange = function () {
         if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200){
             MRT.convertMapJSON(JSON.parse(xhr.responseText));
+            ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
         }
     }
-    xhr.send()
+    xhr.send(null)
 };
 
 var MRT = {
@@ -320,3 +321,184 @@ var MRT = {
         }
     }
 };
+
+class StationItem extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return React.createElement("option", null, this.props.name);
+    }
+}
+
+class StationSelector extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.stations = props.stations.map((station, index) => {
+            return React.createElement(StationItem, {key: index+1, name: station.name})
+        });
+        this.stations.unshift(React.createElement(StationItem, {key: "0", name: ""}));
+    }
+
+    render(){
+        return (
+            React.createElement("select", {onChange: this.props.handler},
+                this.stations
+            )
+        );
+    }
+}
+
+class RouteItem extends React.Component{
+    constructor(props){
+        super(props);
+
+        // Check for first elem. First elem is empty. (Dirty hack up mah sleeves)
+        if (props.route["start_station"] === "") {
+            return;
+        }
+
+        const startStation = props.route["start_station"]["name"];
+        const endStation = props.route["end_station"]["name"];
+        const transStation = props.route["transfer_station"]["name"];
+
+        this.routeInfo = "";
+
+        if (transStation == null){
+            this.routeInfo = startStation + " -> " + endStation;
+        }
+        else {
+            this.routeInfo = startStation + " -> " + transStation + " -> " + endStation;
+        }
+    }
+
+    render(){
+        return React.createElement("option", null, this.routeInfo);
+    }
+}
+
+class RouteSelector extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            React.createElement("select", {onChange: this.props.handler},
+
+                    this.props.routes.map((route) => {
+                        // The only way that I can make a unique key so that React will update properly
+                        const key = route["start_station"]["id"] + "|"   + route["transfer_station"]["id"] + "|" + route["end_station"]["id"];
+                        return React.createElement(RouteItem, {key: key, route: route});
+                    })
+
+            )
+        );
+    }
+}
+
+class TripForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {startStation:'', endStation: '', selectedRoute: null, routes: []};
+        this.handleStartStationChange = this.handleStartStationChange.bind(this);
+        this.handleEndStationChange = this.handleEndStationChange.bind(this);
+        this.handleRouteChange = this.handleRouteChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.getRoutes = this.getRoutes.bind(this);
+    }
+
+    handleStartStationChange(event){
+        this.setState({startStation: event.target.value}, this.getRoutes);
+    }
+
+    handleEndStationChange(event){
+        this.setState({endStation: event.target.value}, this.getRoutes);
+    }
+
+    getRoutes(){
+        const startStation = MRT.getStationByName(this.state.startStation);
+        const endStation = MRT.getStationByName(this.state.endStation);
+
+        const possibleRoutes = MRT.getPossibleRoutes(startStation, endStation);
+        possibleRoutes.unshift({
+            start_station: "",
+            end_station: "",
+            start_line: "",
+            end_line: "",
+            transfer_station: "",
+            station_path: "",
+            movement_flow: ""
+        });
+
+        this.setState({routes: possibleRoutes});
+    }
+
+    handleRouteChange(event){
+        const selectedRouteIndex = event.target.selectedIndex;
+        this.setState({selectedRoute: this.state.routes[selectedRouteIndex]});
+    }
+
+    handleSubmit(event){
+        const route = this.state.selectedRoute;
+
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "/start", true);
+        xhr.send(JSON.stringify(route));
+    }
+
+    render(){
+        return (
+            React.createElement("div", {className: "tripForm"},
+                React.createElement("div", {className: "row"},
+                    React.createElement("h6", null, "👇 Choose starting station 👇")
+                ),
+                React.createElement("div", {className: "row"},
+                    React.createElement("div", {className: "eight columns"},
+                        React.createElement(StationSelector, {id: "startStationSelector", handler: this.handleStartStationChange, stations: this.props.stations})
+                    )
+                ),
+                React.createElement("div", {className: "row"},
+                    React.createElement("h6", null, "👇 Choose destination station 👇")
+                ),
+                React.createElement("div", {className: "row"},
+                    React.createElement("div", {className: "eight columns"},
+                        React.createElement(StationSelector, {id: "endStationSelector", handler: this.handleEndStationChange, stations: this.props.stations})
+                    )
+                ),
+                React.createElement("div", {className: "row"},
+                    React.createElement("h6", null, "👇 Choose a route! 👇")
+                ),
+                React.createElement("div", {className: "row"},
+                    React.createElement("div", {className: "eight columns"},
+                        React.createElement(RouteSelector, {id: "routeSelector", handler: this.handleRouteChange, routes: this.state.routes})
+                    )
+                ),
+                React.createElement("div", {className: "row start"},
+                    React.createElement("input", {type: "submit", value: "Start!", onClick: this.handleSubmit})
+                )
+            )
+        );
+    }
+}
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            React.createElement("div", null,
+                React.createElement("div", {className: "container"},
+                    React.createElement("div", {className: "row"},
+                        React.createElement("img", {src: "./static/img/overflow-logo.png", id: "logo"})
+                    ),
+                    React.createElement(TripForm, {stations: MRT.stations})
+                )
+            )
+        );
+    }
+}
