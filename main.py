cfe4b193b9cf6106cf5c9ad7c4199da20b34a241
Muhammad Shazrin <omfgitsnotshaz@gmail.com>
2017-04-02 16:47:33 +0800

    Add static folder location

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 from railway.route import Route
 from vehicle.train import Train
 
-app = Flask(__name__, static_url_path="/webapp/static")
+app = Flask(__name__, static_url_path="/webapp/static", static_folder="webapp/static")
 
 train = Train()
 
