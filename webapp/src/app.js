56a2d8152ea020551f8caeaf445dd537a17b17f1
Muhammad Shazrin <omfgitsnotshaz@gmail.com>
2017-04-04 13:07:17 +0800

    App js for the real deal fam

---
+++ webapp/src/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Map from './map';
+import { MapUtils } from './utils';
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div className="container">
+                
+            </div>
+        );
+    }
+}
+
+export default App;
