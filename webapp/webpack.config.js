6ac17d070d24e24b530d11023d855bb9fb178cbf
Muhammad Shazrin <omfgitsnotshaz@gmail.com>
2017-04-07 17:20:22 +0800

    Added entry and output params in webpack config

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -1,4 +1,11 @@
+const path = require('path');
+
 module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'static/js'),
+        filename: 'bundle.js'
+    },
     module: {
         rules: [
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
